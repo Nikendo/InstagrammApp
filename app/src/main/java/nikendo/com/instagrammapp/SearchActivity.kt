82c8fa78acce0ccf69ec69547623d46053b074c8
Nikendo <pravdomir42@gmail.com>
2019-02-28 20:08:32 +0700

    Added activity classes

---
+++ app/src/main/java/nikendo/com/instagrammapp/SearchActivity.kt
@@ -0,0 +1,25 @@
+package nikendo.com.instagrammapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SearchActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setMenuOptions()
+    }
+
+    private fun setMenuOptions() {
+        bottomNavigationView.setIconSize(29f, 29f)
+        bottomNavigationView.setTextVisibility(false)
+        bottomNavigationView.enableItemShiftingMode(false)
+        bottomNavigationView.enableShiftingMode(false)
+        bottomNavigationView.enableAnimation(false)
+        for (i in 0 until bottomNavigationView.menu.size()) {
+            bottomNavigationView.setIconTintList(i, null)
+        }
+    }
+}
