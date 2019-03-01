0c352a2fe4cfc9f14291457a166aa37badc8a6ac
Nikendo <pravdomir42@gmail.com>
2019-03-01 22:42:22 +0700

    Added LoginActivity

---
+++ app/src/main/java/nikendo/com/instagrammapp/LoginActivity.kt
@@ -0,0 +1,16 @@
+package nikendo.com.instagrammapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class LoginActivity : AppCompatActivity() {
+
+    private val TAG = "LoginActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        Log.d(TAG, "onCreate")
+    }
+}
