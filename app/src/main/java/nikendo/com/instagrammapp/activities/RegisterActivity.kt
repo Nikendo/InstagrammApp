e37dd52e43ede23ec0ccd8d56dc02564778fb64d
Nikendo <n.shmatov@intecgroup.ru>
2019-03-04 03:09:15 +0700

    Add

--- app/src/main/java/nikendo/com/instagrammapp/activities/RegisterActivity.kt
+++ app/src/main/java/nikendo/com/instagrammapp/activities/RegisterActivity.kt
@@ -1,12 +1,30 @@
 package nikendo.com.instagrammapp.activities
 
+import android.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import nikendo.com.instagrammapp.R
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val TAG = "RegisterActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        Log.d(TAG, "onCreate")
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().add(R.id.frameLayout, EmailFragment()).commit()
+        }
     }
+}
+
+class EmailFragment: Fragment() {
+
+}
+
+class NamePassFragment: Fragment() {
+
 }
