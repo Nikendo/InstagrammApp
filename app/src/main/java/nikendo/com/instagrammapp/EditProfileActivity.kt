7f6b8efbb0ef5c3a3131bc6bb0c44d55ad338a91
Nikendo <n.shmatov@intecgroup.ru>
2019-03-12 01:55:47 +0700

    Add. Updated profile can will saved

--- app/src/main/java/nikendo/com/instagrammapp/EditProfileActivity.kt
+++ app/src/main/java/nikendo/com/instagrammapp/EditProfileActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -12,13 +13,16 @@ import kotlinx.android.synthetic.main.activity_edit_profile.view.*
 import nikendo.com.instagrammapp.activities.ValueEventListenerAdapter
 import nikendo.com.instagrammapp.activities.showToast
 import nikendo.com.instagrammapp.models.User
+import nikendo.com.instagrammapp.views.PasswordDialog
 
-class EditProfileActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity(), PasswordDialog.Listener {
 
     private val TAG = "EditProfileActivity "
+
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mDatabase: DatabaseReference
     private lateinit var mUser: User
+    private lateinit var mPendingUser: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,25 +34,43 @@ class EditProfileActivity : AppCompatActivity() {
         }
         imageSave.setOnClickListener {
             updateProfile()
-            finish()
         }
 
         mAuth = FirebaseAuth.getInstance()
         mDatabase = FirebaseDatabase.getInstance().reference
-        mDatabase.child("users").child(mAuth.currentUser!!.uid).addListenerForSingleValueEvent(ValueEventListenerAdapter {
-            mUser = it.getValue(User::class.java)!!
-            etNameInput.setText(mUser.name, TextView.BufferType.EDITABLE)
-            etUsernameInput.setText(mUser.username, TextView.BufferType.EDITABLE)
-            etWebsiteInput.setText(mUser.website, TextView.BufferType.EDITABLE)
-            etBioInput.setText(mUser.bio, TextView.BufferType.EDITABLE)
-            etEmailInput.setText(mUser.email, TextView.BufferType.EDITABLE)
-            etPhoneInput.setText(mUser.phone, TextView.BufferType.EDITABLE)
-            editProfileToolBar.tvUserName.text = mUser.username
-        })
+
+        mDatabase.child("users").child(mAuth.currentUser!!.uid)
+                .addListenerForSingleValueEvent(ValueEventListenerAdapter {
+                    mUser = it.getValue(User::class.java)!!
+                    etNameInput.setText(mUser.name, TextView.BufferType.EDITABLE)
+                    etUsernameInput.setText(mUser.username, TextView.BufferType.EDITABLE)
+                    etWebsiteInput.setText(mUser.website, TextView.BufferType.EDITABLE)
+                    etBioInput.setText(mUser.bio, TextView.BufferType.EDITABLE)
+                    etEmailInput.setText(mUser.email, TextView.BufferType.EDITABLE)
+                    etPhoneInput.setText(mUser.phone, TextView.BufferType.EDITABLE)
+                    editProfileToolBar.tvUserName.text = mUser.username
+                })
+    }
+
+    override fun onPasswordConfirm(password: String) {
+        val credential = EmailAuthProvider.getCredential(mUser.email, password)
+        mAuth.currentUser!!.reauthenticate(credential).addOnCompleteListener{
+            if (it.isSuccessful) {
+                mAuth.currentUser!!.updateEmail(mPendingUser.email).addOnCompleteListener{
+                    if (it.isSuccessful) {
+                        updateUser(mPendingUser)
+                    } else {
+                        showToast(it.exception!!.message!!)
+                    }
+                }
+            } else {
+                showToast(it.exception!!.message!!)
+            }
+        }
     }
 
     fun updateProfile() {
-        val user = User(
+        mPendingUser = User(
                 name = etNameInput.text.toString(),
                 username = etUsernameInput.text.toString(),
                 website = etWebsiteInput.text.toString(),
@@ -56,14 +78,12 @@ class EditProfileActivity : AppCompatActivity() {
                 email = etEmailInput.text.toString(),
                 phone = etPhoneInput.text.toString()
         )
-        val error = validate(user)
+        val error = validate(mPendingUser)
         if (error == null) {
-            if (user.email == mUser.email) {
-                updateUser(user)
+            if (mPendingUser.email == mUser.email) {
+                updateUser(mPendingUser)
             } else {
-                // re-authenticate
-                // update email in auth
-                // update user
+                PasswordDialog().show(supportFragmentManager, "password dialog")
             }
         } else {
             showToast(error)
@@ -79,9 +99,9 @@ class EditProfileActivity : AppCompatActivity() {
         if (user.email != mUser.email) updatesMap["email"] = user.email
         if (user.phone != mUser.phone) updatesMap["phone"] = user.phone
         mDatabase.child("users").child(mAuth.currentUser!!.uid).updateChildren(updatesMap)
-                .addOnCompleteListener {
+                .addOnCompleteListener{
                     if (it.isSuccessful) {
-                        showToast("Profile save")
+                        showToast("Profile saved")
                         finish()
                     } else {
                         showToast(it.exception!!.message!!)
